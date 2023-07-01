37535d1f56d381237635b01e2efecc33dc5ad993
hayoun <fishingest@kookmin.ac.kr>
2023-07-02 02:04:41 +0900

    [init] Spring 프로젝트 세팅

---
+++ src/main/java/com/RefairTalk_Backend/RefairTalkBackendApplication.java
@@ -0,0 +1,13 @@
+package com.RefairTalk_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefairTalkBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RefairTalkBackendApplication.class, args);
+	}
+
+}
