be0d1da62cab2a98e81ad3a83b963598a3c24c05
hayounSong <fishingest@kookmin.ac.kr>
2023-07-08 21:46:34 +0900

    [init] Exceoption 설정

--- src/main/java/com/RepairTalk_Backend/common/dto/ApiResponseDto.java
+++ src/main/java/com/RepairTalk_Backend/common/dto/ApiResponseDto.java
@@ -1,2 +1,30 @@
-package com.RepairTalk_Backend.common.dto;public class ApiResponseDto {
+package com.RepairTalk_Backend.common.dto;
+
+import com.RepairTalk_Backend.exception.ErrorStatus;
+import com.RepairTalk_Backend.exception.SuccessStatus;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class ApiResponseDto<T>{
+    private final int code;
+    private final String message;
+    private T data;
+
+    public static ApiResponseDto success(SuccessStatus successStatus){
+        return new ApiResponseDto<>(successStatus.getHttpStatus().value(), successStatus.getMessage());
+    }
+
+    public static <T> ApiResponseDto<T> success(SuccessStatus successStatus, T data){
+        return new ApiResponseDto<T>(successStatus.getHttpStatus().value(), successStatus.getMessage(), data);
+    }
+
+    public static ApiResponseDto error(ErrorStatus errorStatus){
+        return new ApiResponseDto<>(errorStatus.getHttpStatus().value(), errorStatus.getMessage());
+    }
+
 }
--- src/main/java/com/RepairTalk_Backend/exception/SuccessStatus.java
+++ src/main/java/com/RepairTalk_Backend/exception/SuccessStatus.java
@@ -1,2 +1,38 @@
-package com.RepairTalk_Backend.exception;public enum SuccessStatus {
+package com.RepairTalk_Backend.exception;
+
+
+/*
+성공 status의 공통 클래스를 작성했습니다.
+ */
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+public enum SuccessStatus {
+
+
+    /**
+     * 200 FIND
+     */
+    SEARCH_SUCCESS(HttpStatus.OK,"조회에 성공했습니다."),
+
+    /*
+    movie
+     */
+    CREATE_SUCCESS(HttpStatus.CREATED, "영화등록이 완료되었습니다."),
+
+    FIND_SUCCESS(HttpStatus.CREATED, "상세조회 성공"),
+    TIME_SUCCESS(HttpStatus.CREATED, "시간조회 성공");
+
+
+    private final HttpStatus httpStatus;
+
+    private final String message;
+
+    public int getHttpStatusCode() {
+        return httpStatus.value();
+    }
 }
